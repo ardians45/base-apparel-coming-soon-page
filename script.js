f6689aed6bbaa870bb126013584a8e3f8d35c218
Ardian Setiawan <ardians1945@gmail.com>
2024-04-26 21:00:36 +0700

    Base Apparel coming soon page

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("emailForm");
+const emailInput = document.getElementById("input-email");
+const input = document.getElementById("input");
+const errorMessage = document.getElementById("error-message");
+const iconError = document.getElementById("icon-error");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = emailInput.value.trim();
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  if (email === "") {
+    errorMessage.textContent = "Please enter your email address.";
+    errorMessage.style.color = "red";
+    input.style.borderColor = "red";
+    iconError.style.visibility = "visible";
+    return;
+  }
+
+  if (!isValidEmail) {
+    errorMessage.textContent = "Please provide a valid email";
+    errorMessage.style.color = "red";
+    input.style.borderColor = "red";
+    emailIcon.style.display = "block";
+    iconError.style.visibility = "visible";
+    return;
+  } else {
+    errorMessage.textContent = "Thanks for signing up!";
+    errorMessage.style.color = "";
+    input.style.borderColor = "";
+    emailInput.value = "";
+    iconError.style.visibility = "hidden";
+    return;
+  }
+});
